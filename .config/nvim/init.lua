d5eaaa15d7499ed98e5c794ccfe869ce3edf1e06
Sbtah <shisastore@protonmail.com>
2024-06-22 12:50:41 +0200

    Rework nvim config.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,3 @@
--- Seting vimrc commands in lua config via vim.cmd
-vim.cmd("set termguicolors")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
-vim.g.mapleader = " "
-
-
 -- LazyVim bootstrap script.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -22,33 +12,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- All plugins that I'm currently using.
-local plugins = {
-    {"Mofiqul/dracula.nvim" }, {"LazyVim/LazyVim", opts = { colorscheme = "dracula", priority = 1000, lazy = false}},
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}, 
-    {"nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = { "nvim-lua/plenary.nvim" }},
-   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
-}
+-- Load options
+require("options")
 
-local opts = {
-}
-
-require("lazy").setup(plugins, opts)
-
---vim.cmd.colorscheme "dracula"
-
--- Treesitter config
-local config = require("nvim-treesitter.configs")
-config.setup(
-    {
-        ensure_installed = {"lua", "python"},
-        highlight = { enable = true },
-        indent = { enable = true },
-    }
-)
-
--- Telescope
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+-- Load plugins.
+require("lazy").setup("plugins")
 
---
+++ .config/nvim/lua/plugins/dracula.lua
@@ -0,0 +1,6 @@
+return {
+    {"Mofiqul/dracula.nvim" }, {"LazyVim/LazyVim", opts = { colorscheme = "dracula", priority = 1000, lazy = false}},
+    config = function()
+        vim.cmd.colorscheme "dracula"
+    end
+}
