9c8331b506fc35b98dc8614a72b23bbdeab18392
Sbtah <shisastore@protonmail.com>
2024-06-22 22:29:09 +0200

    Add more nvim configurations.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,5 +4,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
+vim.cmd("set listchars=space:·,tab:->\"")
+vim.cmd("set list")
 vim.g.mapleader = " "
 
