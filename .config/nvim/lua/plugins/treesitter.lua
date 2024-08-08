ad7a98df443f1896253cd62036e069dc8b15a9d2
Sbtah <shisastore@protonmail.com>
2024-08-08 23:03:38 +0200

    Update config.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,12 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup(
-        {
-        ensure_installed = {"lua", "python"},
-        highlight = { enable = true },
-        indent = { enable = true },
-        }
-    )
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "lua", "python", "javascript" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
-
